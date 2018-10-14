0801ad5143181aa69cb4ceaa7df6e74ad1ed1081
Nikita Karamov <nikita.karamoff@gmail.com>
2018-10-14 21:30:23 +0300

    Collision: Trying to make it work but it won't

--- Collision/main.py
+++ Collision/main.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 import pygame
 
 
@@ -38,6 +40,29 @@ class Line(Shape):
         self.x2, self.y2 = x2, y2
 
 
+def check_collision(rectangle, circle):
+    rectangle_box = (rectangle.x,
+                     rectangle.y,
+                     rectangle.x + rectangle.width,
+                     rectangle.y + rectangle.height)
+    circle_box = (circle.x - circle.radius,
+                  circle.y - circle.radius,
+                  circle.x + circle.radius,
+                  circle.y + circle.radius)
+
+    if rectangle_box[0] > circle_box[2] \
+            or rectangle_box[1] > circle_box[3] \
+            or rectangle_box[2] < circle_box[0] \
+            or rectangle_box[3] < circle_box[1]:
+        return False
+
+    for x in (rectangle_box[0], rectangle_box[2]):
+        for y in (rectangle_box[1], rectangle_box[3]):
+            if sqrt((x - circle.x) ^ 2 + (y - circle.y) ^ 2) <= circle.radius:
+                print(sqrt((x - circle.x) ^ 2 + (y - circle.y) ^ 2), circle.radius)
+                return True
+
+
 # Screen parameters
 BOARD_WID = 500
 BOARD_HGT = 500
@@ -61,14 +86,16 @@ OBJECTS_COLOR = (85, 85, 85)
 TEXT_COLOR = (0, 0, 0)
 
 pygame.init()
+FONT = pygame.font.Font(None, 24)
 
 screen = pygame.display.set_mode((WINDOW_WID, WINDOW_HGT))
 pygame.display.set_caption(WINDOW_CAPTION)
 CLOCK = pygame.time.Clock()
 
 running = True
+collision = False
 
-objects = [Circle(100, 100, 50), Rectangle(400, 400, 100, 100)]
+objects = [Circle(100, 100, 50), Rectangle(200, 200, 100, 100)]
 
 selected_index = 0
 
@@ -102,6 +129,20 @@ while running:
             pygame.draw.circle(screen, OBJECTS_COLOR, (obj.x, obj.y), obj.radius, 0)
         elif type(obj) is Rectangle:
             pygame.draw.rect(screen, OBJECTS_COLOR, [obj.x, obj.y, obj.width, obj.height], 0)
+
+    collision = check_collision(objects[1], objects[0])
+
+    instr_text = FONT.render('Selected object %d' % (selected_index + 1), 1, TEXT_COLOR)
+    instr_text_pos = instr_text.get_rect()
+    instr_text_pos.centerx = BOARD_WID + PADD * 2 + SIDEBAR_WID / 2
+    instr_text_pos.centery = WINDOW_HGT - PADD - instr_text.get_height() / 2
+    screen.blit(instr_text, instr_text_pos)
+
+    coll_text = FONT.render('COLLIDING!' if collision else 'Not colliding', 1, TEXT_COLOR)
+    coll_text_pos = coll_text.get_rect()
+    coll_text_pos.centerx = BOARD_WID + PADD * 2 + SIDEBAR_WID / 2
+    coll_text_pos.centery = WINDOW_HGT / 2
+    screen.blit(coll_text, coll_text_pos)
 
     pygame.display.flip()
     CLOCK.tick(FPS)
